738e8e914dd45cb01e8f09ce786523e9ded3969b
shakirhasan98 <shakirhasan1@outlook.com>
2022-11-14 10:59:57 +0300

    taking input and console

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("script is working from a diff file");
+var btntranslate = document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#txt-input");
+
+console.log(txtInput)
+
+function clickEventHandler() {
+    console.log("clicked!");
+    console.log("input", txtInput.value)
+}
+
+btntranslate.addEventListener("click", clickHandler)
